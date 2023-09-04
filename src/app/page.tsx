30f1cadb1381640cf030a77ccf1e13744e916445
Joao Maranhao <joaofelipemaranhao@gmail.com>
2023-09-04 02:25:33 -0300

    refactor: move navbar to own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,3 @@
-import Image from "next/image"
-import {FaGithub, FaLinkedinIn, FaTwitter} from 'react-icons/fa'
 export default function Home() {
-  return (
-    <main>
-      <nav className="bg-blue-700">
-        <div className="max-w-5xl m-auto flex justify-between items-center p-6">
-          <div>
-            <a href="/">
-              <Image className="w-10" src="/logo.svg" alt="logo" width={100} height={100} /> 
-            </a>
-          </div>
-          <div className="flex">
-            <a className="mr-4" href="https://www.github.com/joaomaranhao">
-              <FaGithub size={30} />
-            </a>
-            <a className="mr-4" href="https://www.linkedin.com/in/joaofmaranhao">
-              <FaLinkedinIn size={30} />
-            </a>
-            <a href="https://www.twitter.com/joaofmaranhao">
-              <FaTwitter size={30} />
-            </a>
-          </div>
-        </div>
-      </nav>
-    </main>
-  )
+  return <main></main>;
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+
+export const Navbar = () => {
+  return (
+    <nav className="bg-blue-700">
+      <div className="max-w-5xl m-auto flex justify-between items-center py-6">
+        <div>
+          <a href="/">
+            <Image
+              className="w-10 hover:scale-110 ease-out duration-300"
+              src="/logo.svg"
+              alt="logo"
+              width={100}
+              height={100}
+            />
+          </a>
+        </div>
+        <div className="flex">
+          <a
+            className="mr-4 hover:scale-110 ease-out duration-300 text-white"
+            href="https://www.github.com/joaomaranhao"
+          >
+            <FaGithub size={30} />
+          </a>
+          <a
+            className="mr-4 hover:scale-110 ease-out duration-300 text-white"
+            href="https://www.linkedin.com/in/joaofmaranhao"
+          >
+            <FaLinkedinIn size={30} />
+          </a>
+          <a
+            className="hover:scale-110 ease-out duration-300 text-white"
+            href="https://www.twitter.com/joaofmaranhao"
+          >
+            <FaTwitter size={30} />
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
